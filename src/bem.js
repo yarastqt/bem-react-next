14377ae234e90bc6e667a97e0155fce7072594bc
yarastqt <yarastqt@gmail.com>
2018-02-26 17:03:53 +0300

    feat(bem): change to dump component

--- src/bem.js
+++ src/bem.js
@@ -1,25 +1,16 @@
-import { isNil, omit, merge } from 'ramda'
+import { createElement } from 'react'
 
-import { BemComponent } from './component'
+import { buildClassName } from './utils/class-name-builder'
 
 
-// TODO: Change to dump component
-export class Bem extends BemComponent {
-  constructor(props) {
-    super(props)
-    this.block = props.block
-
-    if (!isNil(props.tag)) {
-      this.tag = props.tag
-    }
-  }
-
-  attrs() {
-    // Return attrs without omit keys
-    return omit([
-      'tag',
-      'block',
-      'attrs',
-    ], merge(this.props.attrs, this.props))
-  }
+export function Bem({ tag = 'div', block, children, attrs, ...props }) {
+  return createElement(tag, {
+    ...attrs,
+    children,
+    className: buildClassName({
+      block,
+      mods: {},
+    }),
+    ...props,
+  })
 }
