f61a898f3eb67c59020d84dd2c3c42fb3fbbf762
yarastqt <yarastqt@gmail.com>
2018-03-23 16:18:19 +0300

    feat(bem): add prop-types, add support elem and mods props

--- src/bem.js
+++ src/bem.js
@@ -1,16 +1,35 @@
 import { createElement } from 'react'
+import { string, node, shape } from 'prop-types'
 
 import { buildClassName } from './utils/class-name-builder'
 
 
-export function Bem({ tag = 'div', block, children, attrs, ...props }) {
+export function Bem({ tag, block, elem, mods, children, attrs, ...props }) {
   return createElement(tag, {
     ...attrs,
     children,
     className: buildClassName({
       block,
-      mods: {},
+      elem,
+      mods,
     }),
     ...props,
   })
+}
+
+Bem.propTypes = {
+  tag: string,
+  block: string.isRequired,
+  elem: string,
+  mods: shape({}),
+  children: node,
+  attrs: shape({}),
+}
+
+Bem.defaultProps = {
+  tag: 'div',
+  elem: undefined,
+  mods: {},
+  children: null,
+  attrs: {},
 }
